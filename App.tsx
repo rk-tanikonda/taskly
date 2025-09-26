7a571c1657f55f46f0bac01e702da98ab592e737
rk-tanikonda <rk.tanikonda@gmail.com>
2025-09-27 00:40:56 +0530

    Add delete functionality and theme support in App component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native'
+import { theme } from './theme'
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert('Delete', 'Are you sure you want to delete this item?', [
+      {
+        text: 'Delete',
+        style: 'destructive',
+        onPress: () => console.log('Deleted'),
+      },
+      { text: 'Cancel', style: 'cancel' },
+    ])
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style='auto' />
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity
+          onPress={handleDelete}
+          activeOpacity={0.5}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
@@ -13,8 +32,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: theme.colors.white,
     justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: theme.colors.black,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colors.white,
+    fontSize: 16,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    letterSpacing: 1,
+  },
+  itemContainer: {
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    borderBottomWidth: 2,
+    borderBottomColor: theme.colors.cerulean,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  itemText: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 })
---
+++ theme.ts
@@ -0,0 +1,7 @@
+export const theme = {
+  colors: {
+    cerulean: '#1a759f',
+    white: '#fff',
+    black: '#000',
+  },
+}
