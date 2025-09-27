2099abae74f556bdb35cb6b18398c469e4147422
rk-tanikonda <rk.tanikonda@gmail.com>
2025-09-27 10:46:33 +0530

    Add notification scheduling feature and update dependencies
    - Added expo-device and expo-notifications to package.json for push notification support.
    - Implemented a button in the CounterScreen component to schedule a greeting notification.
    - Enhanced user experience by alerting users if notification permissions are not granted.

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,9 +1,43 @@
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity, Alert } from 'react-native'
+import * as Notifications from 'expo-notifications'
+import * as Device from 'expo-device'
+import { registerForPushNotificationsAsync } from '../../utils/registerForPushNotificationsAsync'
+import { theme } from '../../theme'
 
 export default function CounterScreen() {
+  const scheduleNotification = async () => {
+    const result = await registerForPushNotificationsAsync()
+    console.log('Permission: ', result)
+    if (result === 'granted') {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'Hello!',
+          body: 'Welcome to the app! This is your greeting notification.',
+        },
+        trigger: {
+          type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+          seconds: 5,
+        },
+      })
+    } else {
+      if (Device.isDevice) {
+        Alert.alert(
+          'unable to schedule notification',
+          'Enable the notifications permission for Expo Go in settings'
+        )
+      }
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Counter</Text>
+      <TouchableOpacity
+        onPress={scheduleNotification}
+        style={styles.button}
+        activeOpacity={0.8}
+      >
+        <Text style={styles.buttonText}>Schedule notification</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -17,5 +51,17 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 24,
+  },
+  button: {
+    padding: 12,
+    backgroundColor: theme.colors.black,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colors.white,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    letterSpacing: 1,
+    fontSize: 16,
   },
 })
